e629317ae5c4fc0a8a4562457e2acebf25252ac2
YiFei Zhu <zhuyifei1999@gmail.com>
2019-02-27 14:46:07 -0600

    Read login username from environ

--- user-config.py
+++ user-config.py
@@ -1,6 +1,11 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
+import os as _os
+
 maxlag = 0  # This script is interactive
 
 mylang = family = 'commons'  # silence pywikibot warning
+
+if 'PYWIKIBOT_USERNAME' in _os.environ:
+    usernames['commons']['commons'] = _os.environ['PYWIKIBOT_USERNAME']
